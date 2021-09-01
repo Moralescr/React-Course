a3414449fb01b1312384e71c20e9263f51340d78
Adrian Morales Guadamuz <moralesguadamuz@gmail.com>
2021-08-31 22:48:38 -0600

    Counter

---
+++ 03-counter-app/src/CounterApp.js
@@ -0,0 +1,38 @@
+//Para crear todo el componente se coloca rafcp
+
+import React, {useState} from 'react';
+import PropTypes from 'prop-types'
+
+const CounterApp = ( { numero } ) => {
+
+    const [counter, setCounter ] = useState(numero);
+
+    const handleAdd = () => {
+        setCounter( counter + 1);
+        // setCounter( (c) => c + 1 );
+    }
+
+    const handleSubtract = () =>{
+        setCounter( counter - 1 );
+    }
+
+    const handleReset = () => {
+        setCounter( numero );
+    }
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2> { counter } </h2>
+            <button onClick = { handleAdd }> +1 </button>
+            <button onClick = { handleReset }> Reset </button>
+            <button onClick = { handleSubtract }> -1 </button>
+        </>
+    );
+}
+
+CounterApp.propTypes = {
+    numero: PropTypes.number
+}
+
+export default CounterApp;
